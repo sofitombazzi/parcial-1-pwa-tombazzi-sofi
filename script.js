65ee05a681c07c9fda563ac77d4d7d775886e91e
Sofia Tombazzi <128006502+sofitombazzi@users.noreply.github.com>
2024-05-01 07:28:39 -0300

    feat: creo las constantes para las url de donde me voy a traer datos

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//https://pokeapi.co/api/v2/pokemon?limit=100&offset=0
+//Creo mis constantes para guardar las URL de donde voy a traer datos
+const URL_ENDPOINT = "https://pokeapi.co/api/v2/pokemon/";
+const URL_POKEMON = URL_ENDPOINT + "?limit=100&offset=0";
+
+//Pruebo que ande
+console.log(URL_POKEMON);
+
